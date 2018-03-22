475112c8fdd438dd040273a74220a0efc5e4eca2
Procrastinatorhater <procrastinatorhater@gmail.com>
2018-03-22 16:44:58 -0600

    Initial commit.

---
+++ ExampleIkonAdapter/ExampleAdapter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.AddIn;
+using IkonApplianceAdapterAddInCommon;
+
+namespace IkonAdapterExample
+{
+    [AddIn("Example Adapter", Version = "1.0.0.0", Publisher = "YOURNAME - YOURCOMPANY", Description = "Example Adapter Description")]
+    public class ExampleAdapter : IkonApplianceAdapterBase
+    {
+        /// <summary>
+        /// Main entry point for Addin Pipeline.
+        /// </summary>
+        /// <param name="passDataObject">PassDataObject</param>
+        /// <returns>Success Message</returns>
+        public override string ProcessPassData(PassDataObject passDataObject)
+        {
+            throw new NotImplementedException("This Adapter has not been implemented yet.");
+        }
+    }
+}
